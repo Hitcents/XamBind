e770c135c084402658fa85095a36b524f7c58f13
Bryan Phillips <bryan.phillips@hitcents.com>
2013-12-19 16:12:34 -0600

    The tests, zey are running!

--- XamBind.Droid.Tests/BoundActivityTests.cs
+++ XamBind.Droid.Tests/BoundActivityTests.cs
@@ -34,17 +34,63 @@ namespace XamBind.Droid.Tests
 			_activity.Bind(context, _view);
 		}
 
-		[Test]
-		public void PropertyToTextView()
-		{
-			var textView = _view.FindViewById<TextView>(Resource.Id.Text);
-			for (int i = 0; i < Times; i++)
-			{
-				_viewModel.Text = i.ToString();
+        [Test]
+        public void PropertyToTextView()
+        {
+            var textView = _view.FindViewById<TextView>(Resource.Id.Text);
+            for (int i = 0; i < Times; i++)
+            {
+                _viewModel.Text = i.ToString();
+                Assert.AreEqual(_viewModel.Text, textView.Text);
+            }
+        }
 
-				Assert.AreEqual(_viewModel.Text, textView.Text);
-			}
-		}
+        [Test]
+        public void ButtonCanClick()
+        {
+            var button = _view.FindViewById<Button>(Resource.Id.Search);
+            for (int i = 0; i < Times; i++)
+            {
+                _viewModel.CanSearch = !_viewModel.CanSearch;
+                Assert.AreEqual(_viewModel.CanSearch, button.Enabled);
+            }
+        }
+
+        [Test]
+        public void ButtonClick()
+        {
+            var button = _view.FindViewById<Button>(Resource.Id.Search);
+            for (int i = 0; i < Times; i++)
+            {
+                _viewModel.Searched = false;
+                button.PerformClick();
+                Assert.IsTrue(_viewModel.Searched);
+            }
+        }
+
+        [Test]
+        public void PropertyToTextField()
+        {
+            var editText = _view.FindViewById<EditText>(Resource.Id.TextField);
+            for (int i = 0; i < Times; i++)
+            {
+                _viewModel.TextField = i.ToString();
+
+                Assert.AreEqual(_viewModel.TextField, editText.Text);
+            }
+        }
+
+        [Test]
+        public void TextFieldToProperty()
+        {
+            var editText = _view.FindViewById<EditText>(Resource.Id.TextField);
+            for (int i = 0; i < Times; i++)
+            {
+                editText.Text = i.ToString();
+
+                Assert.AreEqual(editText.Text, _viewModel.TextField);
+            }
+        }
     }
 }
 
--- XamBind.Droid/BindingExtensions.cs
+++ XamBind.Droid/BindingExtensions.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using Android.Content;
 using Android.Views;
 using Android.Widget;
+using XamBind.Reflection;
 
 namespace XamBind
 {
@@ -22,23 +24,43 @@ namespace XamBind
 			var observer = new PropertyObserver(viewModel);
 			bindable.Observer = observer;
 
+            var viewGroup = view as ViewGroup;
+            if (viewGroup != null)
+            {
+                BindChildren(bindable, context, viewGroup);
+            }
+
 			int id = view.Id;
 			if (id == -1)
 				return;
 
 			string name = context.Resources.GetResourceName(id);
+            name = name.Split('/').Last();
 
-			var textView = view as TextView;
-			if (textView != null)
-			{
-				bindable.Observer.Add<string>(name, text => textView.Text = text);
-			}
+            var button = view as Button;
+            if (button != null)
+            {
+                bindable.Observer.Add<bool>("Can" + name, enabled => button.Enabled = enabled);
 
-			var viewGroup = view as ViewGroup;
-			if (viewGroup != null)
-			{
-				BindChildren(bindable, context, viewGroup);
-			}
+                button.Click += (sender, e) => bindable.Observer.InvokeMethod(name);
+                return;
+            }
+
+            var editText = view as EditText;
+            if (editText != null)
+            {
+                bindable.Observer.Add<string>(name, text => editText.Text = text);
+
+                //editText.TextChanged += (sender, e) => viewModel.SetProperty(name, editText.Text);
+                return;
+            }
+
+            var textView = view as TextView;
+            if (textView != null)
+            {
+                bindable.Observer.Add<string>(name, text => textView.Text = text);
+                return;
+            }
 		}
 
 		private static void BindChildren(IBindable bindable, Context context, ViewGroup parent)
